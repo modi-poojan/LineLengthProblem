cb1e5213b9f9255befc7560dda3cbc7c56d345be
“Poojanmodi” <modipoojan444@gmail.com>
2021-11-12 23:24:19 +0530

    UseCase4 OOP's concepts are implemented

--- src/com/LineLength/Line.java
+++ src/com/LineLength/Line.java
@@ -1,43 +1,89 @@
 package com.LineLength;
 
+import java.util.Scanner;
+	
 public class Line {
-
-	public static void main(String[] args) {
+	
+	double x1, x2, x3, x4;
+	double y1, y2, y3, y4;
+	double length;
+	
+	public double equals() {
 		
-		double negative = Math.floor(Math.random() * 2);		
-		double x1 = Math.floor(Math.random() * 10);
+		double length2 = Math.sqrt( Math.pow((x4 - x3), 2) + Math.pow((y4 - y3), 2));
 		
-		if (negative == 1) {
-			x1 *= -1;
+		if (length == length2) {
+			System.out.println(" The two lines are equal ");
+		}
+		else {
+			System.out.println(" The two lines are not equal");
 		}
 		
-		negative = Math.floor(Math.random() * 2);
-		double y1 = Math.floor(Math.random() * 10);
-		if (negative == 1) {
-			y1 *= -1;
+		return length2;
+	}
+	
+	public void compareTo (double length, double length2) {
+		
+		if (length == length2) {
+			System.out.println("\n The two lines are equal in length");
 		}
 		
-		negative = Math.floor(Math.random() * 2);
-		double x2 = Math.floor(Math.random() * 10);
-		if (negative == 1) {
-			x2 *= -1;
+		else if (length > length2) {
+			System.out.println("\n The first line is greater than the second. ");
 		}
 		
-		negative = Math.floor(Math.random() * 2);
-		double y2 = Math.floor(Math.random() * 10);
-		if (negative == 1) {
-			y2 *= -1;
+		else {
+			System.out.println("\n The second line is greater than the first. ");
 		}
+	}
+	
+	public void calculateLength() {
+		// TODO Auto-generated method stub
 		
-		double length = Math.sqrt( Math.pow((x2 - x1), 2) + Math.pow((y2 - y1), 2));
+		Scanner read = new Scanner(System.in);
 		
-		System.out.println(" The first point is : (" + x1 + ", " + y1 + ")");
-		System.out.println(" The second point is : (" + x2 + ", " + y2 + ")");
-		System.out.println(" The length of the line is : " + length);
+		System.out.print(" Enter x1: ");
+		x1 = read.nextDouble();
+		
+		System.out.print(" Enter y1: ");
+		y1 = read.nextDouble();
+		
+		System.out.print(" Enter x2: ");
+		x2 = read.nextDouble();
+		
+		System.out.print(" Enter y2: ");
+		y2 = read.nextDouble();
+		
+		System.out.print(" Enter x3: ");
+		x3 = read.nextDouble();
+		
+		System.out.print(" Enter y3: ");
+		y3 = read.nextDouble();
+		
+		System.out.print(" Enter x4: ");
+		x4 = read.nextDouble();
+		
+		System.out.print(" Enter y4: ");
+		y4 = read.nextDouble();
+		
+		length = Math.sqrt( Math.pow((x2 - x1), 2) + Math.pow((y2 - y1), 2));
+		
+		System.out.println("\n The first point is : (" + x1 + ", " + y1 + ")");
+		System.out.println(" The second point is : (" + x2 + ", " + y2 + ")");		
+		
+		System.out.println(" The third point is : (" + x3 + ", " + y3 + ")");
+		System.out.println(" The fourth point is : (" + x4 + ", " + y4 + ")\n");
+		
+		
+		
+		
+		double length2 = equals();
+		System.out.println(" The length of the first line is : " + length);
+		System.out.println(" The length of the second line is: " + length2);
+		
+		compareTo(length, length2);		
+		
 	}
 
-
-
-	}
-
+}
 
---
+++ src/com/LineLength/Test.java
@@ -0,0 +1,11 @@
+package com.LineLength;
+
+public class Test {
+
+	public static void main(String[] args) {
+		Line obj = new Line();
+		obj.calculateLength();
+
+	}
+
+}
